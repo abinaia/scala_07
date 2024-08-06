7f06d99fbe9d3610b2be3d59b6b67437693a8d9e
Abinaia Kuganesan <abinaiakugan@gmail.com>
2024-08-06 08:24:56 +0530

    updated

--- Q2.scala
+++ Q2.scala
@@ -1,12 +1,11 @@
-
 object Q2{
 
 def calculateSquare(numbers:List[Int]):List[Int]={
     numbers.map(num=>num*num)
 }
   def main(args: Array[String]): Unit = {
-    val inputlist = List(1,2,3,4,5,6,7,8,9,10)
-    println(calculateSquare(inputlist))
+    val input = List(1,2,3,4,5,6,7,8,9,10)
+    println(calculateSquare(input))
    
   }
 }
